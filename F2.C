03d92350b7b1240b92f154eba78fc0bc76e65b7d
Tarun <atypicalesper@gmail.com>
2020-10-03 13:07:40 +0530

    Update F2.C

--- F2.C
+++ F2.C
@@ -3,8 +3,8 @@
 void main()
 { int gd=0,gm;
  initgraph(&gm,&gd,"C:\\turboc++\\disk\\turboc3\\bgl");
- setcolor(1);
- printf("hello C");
+ setcolor(1); //to determine the color
+ printf("hello C"); //to display some thing on the screen
  getch();
  closegraph();
 
